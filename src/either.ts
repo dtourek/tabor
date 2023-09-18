297c68e9cbf51a754c0aec639de7ac753b6fd0da
davidtourek <dtourek@centrum.cz>
2023-09-18 11:23:42 +0200

    fail renamed to failed

--- src/either.ts
+++ src/either.ts
@@ -1,9 +1,9 @@
-import { IFail, isFail, ISuccess, IMaybe } from './maybe';
+import { IFail, isFailed, ISuccess, IMaybe } from './maybe';
 
 export type IEither<A, B> = IFail<A> | ISuccess<B>;
 
 export const either = <OnError, OnSuccess>(onError: (error: Error) => OnError, onSuccess: (value: OnSuccess) => any, condition: IMaybe<OnSuccess>) => {
-  if (isFail(condition)) {
+  if (isFailed(condition)) {
     return onError(condition.value);
   }
   return onSuccess(condition.value);
